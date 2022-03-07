7c3c8a0d7b313026c08ff5808cf94e89a8b5d347
Alexey Abramov <abramov.al3x3y@gmail.com>
2022-03-07 12:53:15 +0300

    feat: add routes

--- remember-words-app/src/app/app-routing.module.ts
+++ remember-words-app/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
+import { AddWordComponent } from './add-word/add-word.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VocabularyComponent } from './vocabulary/vocabulary.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'vocabulary',
+    component: VocabularyComponent,
+    children: [
+      { path: 'add', component: AddWordComponent },
+      { path: ':id', component: AddWordComponent },
+    ],
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- remember-words-app/src/app/app.module.ts
+++ remember-words-app/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VocabularyComponent } from './vocabulary/vocabulary.component';
+import { WordComponent } from './word/word.component';
+import { AddWordComponent } from './add-word/add-word.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VocabularyComponent,
+    WordComponent,
+    AddWordComponent,
+    SettingsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
