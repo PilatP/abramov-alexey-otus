092ca04c9dbcd271ead2e3c2460ff0dc8d54566a
Alexey Abramov <abramov.al3x3y@gmail.com>
2022-03-20 20:20:03 +0300

    feat: add store

--- calculator/src/store/index.ts
+++ calculator/src/store/index.ts
@@ -1,9 +1,74 @@
-import { createStore } from "vuex";
+import { InjectionKey } from "vue";
+import { createStore, useStore as baseUseStore, Store } from "vuex";
 
-export default createStore({
-  state: {},
+interface IAppState {
+  trainingDays: number;
+  accuracy: number;
+  lastResult: {
+    tasks: number;
+    taskSolved: number;
+  };
+}
+
+export type ActionTypes = "add" | "sub" | "multiple" | "division" | "power";
+interface ISettingsState {
+  duration: number;
+  complexity: number;
+  activatedActions: ActionTypes[];
+}
+
+export interface IState {
+  app: IAppState;
+  settings: ISettingsState;
+}
+export const store = createStore<IState>({
+  state: {
+    app: {
+      trainingDays: 1,
+      accuracy: 100,
+      lastResult: {
+        tasks: 0,
+        taskSolved: 0,
+      },
+    },
+    settings: {
+      duration: 10,
+      complexity: 3,
+      activatedActions: ["add", "sub"],
+    },
+  },
   getters: {},
-  mutations: {},
+  mutations: {
+    toggleAction(state, payload: ActionTypes) {
+      const actionExist = state.settings.activatedActions.some(
+        (action) => action === payload
+      );
+      if (actionExist)
+        state.settings.activatedActions =
+          state.settings.activatedActions.filter(
+            (action) => action !== payload
+          );
+      else state.settings.activatedActions.push(payload);
+    },
+    updateDuration(state, payload: number) {
+      state.settings.duration = payload;
+    },
+    updateComplexity(state, payload: number) {
+      state.settings.complexity = payload;
+    },
+    finishTask(state) {
+      state.app.lastResult.taskSolved += 1;
+    },
+    newTask(state) {
+      state.app.lastResult.tasks += 1;
+    },
+  },
   actions: {},
   modules: {},
 });
+
+export const key: InjectionKey<Store<IState>> = Symbol();
+
+export const useStore = () => {
+  return baseUseStore(key);
+};
